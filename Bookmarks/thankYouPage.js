136b55414c6f76ea0e2f95eb7dcb32654697314b
peterj7 <pjeong7@gmail.com>
2017-05-08 17:28:36 -0400

    Update thankYouPage.js

--- Bookmarks/thankYouPage.js
+++ Bookmarks/thankYouPage.js
@@ -1,1 +1,14 @@
-javascript:a=((document.location.href).split('.html')[0]); b=(a.split('content/jmp')[1]); (function(){if(document.location.href.indexOf("aemauthor")>=0&& document.location.href.indexOf("ondemand")>=0){document.location.href='http://aemauthor.unx.sas.com:4502/cf#/content/jmp'+b+'/watch.html';}else if(document.location.href.indexOf("ondemand")>=0){document.location.href= a+'/watch.html';}else if(document.location.href.indexOf("aemauthor")>=0){document.location.href= a+'/thanks.html';}else{document.location.href= a+'/thanks.html';}})();
+javascript:
+a=((document.location.href).split('.html')[0]);
+b=(a.split('content/jmp')[1]);
+function(){
+  if(document.location.href.indexOf("aemauthor")>=0&& document.location.href.indexOf("ondemand")>=0){
+    document.location.href='http://aemauthor.unx.sas.com:4502/cf#/content/jmp'+b+'/watch.html';
+  }else if(document.location.href.indexOf("ondemand")>=0){
+    document.location.href= a+'/watch.html';
+  }else if(document.location.href.indexOf("aemauthor")>=0){
+    document.location.href= a+'/thanks.html';
+  }else{
+    document.location.href= a+'/thanks.html';
+  }
+}();
