50e3bd3ff060231941ace12ac606bdb1af5ed9d5
peterj7 <pjeong7@gmail.com>
2017-05-08 17:25:47 -0400

    Update countrySwitch.js

--- Bookmarks/countrySwitch.js
+++ Bookmarks/countrySwitch.js
@@ -3,4 +3,9 @@ a=((document.location.href).indexOf('_'));
 b=(document.location.href).substring((a-2),(a+3));
 c=(document.location.href).split(b);
 function(){
-  if(document.location.href.indexOf("aem")>=0){document.location.href='http://aemauthor.unx.sas.com:4502/cf#/content/jmp/en_us'+c[1];}else{document.location.href=c[0]+'en_us'+c[1];}})();
+  if(document.location.href.indexOf("aem")>=0){
+    document.location.href='http://aemauthor.unx.sas.com:4502/cf#/content/jmp/en_us'+c[1];
+  } else{
+    document.location.href=c[0]+'en_us'+c[1];
+  }
+}();
