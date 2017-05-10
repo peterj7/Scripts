feff56c1948f7e7ea80e7edac8721e3fa2d56b5b
peterj7 <pjeong7@gmail.com>
2017-05-10 10:33:38 -0400

    Update thankYouPage.js

--- Bookmarks/thankYouPage.js
+++ Bookmarks/thankYouPage.js
@@ -1,14 +1,14 @@
 javascript:
 a=((document.location.href).split('.html')[0]);
-b=(a.split('content/jmp')[1]);
+b=(a.split('INSERT UNIQUE IDENTIFIER HERE')[1]);
 function(){
-  if(document.location.href.indexOf("aemauthor")>=0&& document.location.href.indexOf("ondemand")>=0){
-    document.location.href='INSERT BEGINNING URL HERE'+b+'/watch.html';
-  }else if(document.location.href.indexOf("ondemand")>=0){
-    document.location.href= a+'/watch.html';
-  }else if(document.location.href.indexOf("aemauthor")>=0){
-    document.location.href= a+'/thanks.html';
+  if(document.location.href.indexOf("INSERT UNIQUE IDENTIFIER HERE")>=0&& document.location.href.indexOf("INSERT UNIQUE IDENTIFIER HERE")>=0){
+    document.location.href='INSERT BEGINNING URL HERE'+b+'/INSERT UNIQUE IDENTIFIER HERE';
+  }else if(document.location.href.indexOf("INSERT UNIQUE IDENTIFIER HERE")>=0){
+    document.location.href= a+'/INSERT UNIQUE IDENTIFIER HERE';
+  }else if(document.location.href.indexOf("INSERT UNIQUE IDENTIFIER HERE")>=0){
+    document.location.href= a+'/INSERT UNIQUE IDENTIFIER HERE';
   }else{
-    document.location.href= a+'/thanks.html';
+    document.location.href= a+'/INSERT UNIQUE IDENTIFIER HERE';
   }
 }();
