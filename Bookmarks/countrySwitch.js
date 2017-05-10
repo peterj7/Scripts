55c61fb9ef15555933076c95e64e1342010134ed
peterj7 <pjeong7@gmail.com>
2017-05-10 10:30:30 -0400

    Update countrySwitch.js

--- Bookmarks/countrySwitch.js
+++ Bookmarks/countrySwitch.js
@@ -4,8 +4,8 @@ b=(document.location.href).substring((a-2),(a+3));
 c=(document.location.href).split(b);
 function(){
   if(document.location.href.indexOf("aem")>=0){
-    document.location.href='INSERT BEGINNING URL HERE'+'/en_us'+c[1];
+    document.location.href='INSERT BEGINNING URL HERE'+'/INSERT COUNTRY CODE HERE'+c[1];
   } else{
-    document.location.href=c[0]+'en_us'+c[1];
+    document.location.href=c[0]+'INSERT COUNTRY CODE HERE'+c[1];
   }
 }();
