c3b507e3525cd37f5b6e754813ea742238d99974
peterj7 <pjeong7@gmail.com>
2017-05-10 10:27:42 -0400

    Update dsCountrySwitch.js

--- Bookmarks/dsCountrySwitch.js
+++ Bookmarks/dsCountrySwitch.js
@@ -3,7 +3,7 @@ function(){
   if(document.location.href.indexOf("aem")>=0){
     a=((document.location.href).indexOf('jmp-discovery-summit'));
     b=(document.location.href).substring((a+20),(a+24));
-    c=(document.location.href).split(b);document.location.href='http://aemauthor.unx.sas.com/cf#/content/jmp-discovery-summit/en/'+c[1];
+    c=(document.location.href).split(b);document.location.href='INSERT BEGINNING URL HERE'+'/en/'+c[1];
   }else{
     a=((document.location.href).indexOf('.jmp'));
     b=(document.location.href).substring((a+4),(a+8));
