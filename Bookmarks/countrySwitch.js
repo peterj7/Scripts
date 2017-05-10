d6c01d9f0d805e9135dd0aace3a52b38cefc1b85
peterj7 <pjeong7@gmail.com>
2017-05-10 10:28:21 -0400

    Update countrySwitch.js

--- Bookmarks/countrySwitch.js
+++ Bookmarks/countrySwitch.js
@@ -4,7 +4,7 @@ b=(document.location.href).substring((a-2),(a+3));
 c=(document.location.href).split(b);
 function(){
   if(document.location.href.indexOf("aem")>=0){
-    document.location.href='http://aemauthor.unx.sas.com:4502/cf#/content/jmp/en_us'+c[1];
+    document.location.href='INSERT BEGINNING URL HERE'+'/en_us'+c[1];
   } else{
     document.location.href=c[0]+'en_us'+c[1];
   }
