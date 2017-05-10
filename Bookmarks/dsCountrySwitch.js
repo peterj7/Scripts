bfef2d3db75a473b8622075f2186714873632c67
peterj7 <pjeong7@gmail.com>
2017-05-10 10:31:56 -0400

    Update dsCountrySwitch.js

--- Bookmarks/dsCountrySwitch.js
+++ Bookmarks/dsCountrySwitch.js
@@ -1,12 +1,12 @@
 javascript:
 function(){
-  if(document.location.href.indexOf("aem")>=0){
-    a=((document.location.href).indexOf('jmp-discovery-summit'));
+  if(document.location.href.indexOf("INSERT UNIQUE IDENTIFIER HERE")>=0){
+    a=((document.location.href).indexOf('INSERT UNIQUE IDENTIFIER HERE'));
     b=(document.location.href).substring((a+20),(a+24));
-    c=(document.location.href).split(b);document.location.href='INSERT BEGINNING URL HERE'+'/en/'+c[1];
+    c=(document.location.href).split(b);document.location.href='INSERT BEGINNING URL HERE'+'/INSERT COUNTRY CODE HERE/'+c[1];
   }else{
-    a=((document.location.href).indexOf('.jmp'));
+    a=((document.location.href).indexOf('INSERT UNIQUE IDENTIFIER HERE'));
     b=(document.location.href).substring((a+4),(a+8));
-    c=(document.location.href).split(b);document.location.href=c[0]+'/en/'+c[1];
+    c=(document.location.href).split(b);document.location.href=c[0]+'/INSERT COUNTRY CODE HERE/'+c[1];
   }
 }();
